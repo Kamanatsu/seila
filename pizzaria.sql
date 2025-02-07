e928824ece2b9d69ea5deb96715772d85d94bce7
Kamanatsu <109121926+Kamanatsu@users.noreply.github.com>
2025-02-07 13:50:01 -0300

    Update pizzaria.sql

--- pizzaria.sql
+++ pizzaria.sql
@@ -1,58 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 07/02/2025 às 07:30
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE Pizzaria;
+USE Pizzaria;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE Usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
 
---
--- Banco de dados: `pizzaria`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `cliente`
---
-
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `endereco` varchar(255) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `bairro` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `pedido`
---
+CREATE TABLE Cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
 
 CREATE TABLE `pedido` (
   `id_cliente` int(20) NOT NULL,
   `valor_total` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `pizza`
---
+);
 
 CREATE TABLE `pizza` (
   `id` int(11) NOT NULL,
@@ -61,35 +27,12 @@ CREATE TABLE `pizza` (
   `sabor` varchar(100) NOT NULL,
   `preco` decimal(10,2) NOT NULL,
   `status` enum('Preparando','Assando','Entregue') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuario`
---
-
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `usuario`
---
+);
 
 INSERT INTO `usuario` (`id`, `username`, `senha`) VALUES
 (1, 'ademiro', '1234');
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices de tabela `cliente`
---
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`id`);
 
